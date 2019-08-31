86dd2f3978265bee00f1213407a72668b49603cd
Hoang Phuc Nguyen Vu <hoang-phuc.nguyen-vu@ant-tech.eu>
2019-08-31 20:00:14 +0700

    add animation

---
+++ assets/js/animation.js
@@ -0,0 +1,18 @@
+var animData1 = {
+  wrapper: document.getElementById("lottie1"),
+  animType: "svg",
+  loop: true,
+  prerender: true,
+  autoplay: true,
+  path: "https://assets6.lottiefiles.com/datafiles/40aX5db74VvGPWw/data.json"
+};
+var animData2 = {
+  wrapper: document.getElementById("lottie2"),
+  animType: "svg",
+  loop: true,
+  prerender: true,
+  autoplay: true,
+  path: "https://assets6.lottiefiles.com/packages/lf20_S9kP4W.json"
+};
+var anim1 = bodymovin.loadAnimation(animData1);
+var anim2 = bodymovin.loadAnimation(animData2);
--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,13 @@ window.onscroll = function(e) {
   p = (s / (d - w)) * 100;
   document.getElementsByClassName("progress-bar")[0].value = p;
 };
+function submitForm() {
+  const name = document.getElementById("name").value;
+  const subject = document.getElementById("subject").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+  const data = { name, subject, email, message };
+  console.log(data);
+  $.post("test.php", data);
+  return false;
+}
