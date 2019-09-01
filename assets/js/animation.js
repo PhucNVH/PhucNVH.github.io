b023ffb35d2f3c81901bd4443a65a7df7c51b507
Hoang Phuc Nguyen Vu <hoang-phuc.nguyen-vu@ant-tech.eu>
2019-09-01 13:51:30 +0700

    add some block

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -14,5 +14,14 @@ var animData2 = {
   autoplay: true,
   path: "https://assets6.lottiefiles.com/packages/lf20_S9kP4W.json"
 };
+var animData3 = {
+  wrapper: document.getElementById("lottie3"),
+  animType: "svg",
+  loop: true,
+  prerender: true,
+  autoplay: true,
+  path: "https://assets6.lottiefiles.com/packages/lf20_kDH7yy.json"
+};
 var anim1 = bodymovin.loadAnimation(animData1);
 var anim2 = bodymovin.loadAnimation(animData2);
+var anim2 = bodymovin.loadAnimation(animData3);
